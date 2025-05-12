15422248ae4f8934679cb021c572d2804ea0c565
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-12 21:11:54 +0200

    Add Controller and logic for recieving request for fetching specific pet and error handling.

---
+++ Controllers/PetsController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class PetsController : ControllerBase
+{
+    private readonly IPetService petService;
+
+    public PetsController(IPetService petService)
+    {
+        this.petService = petService;
+    }
+
+    [HttpGet("get-pet/{id}")]
+    public async Task<IActionResult> GetPet(int id)
+    {
+        try
+        {
+            var pet = await petService.FetchPetAsync(id);
+
+            if (pet == null)
+            {
+                return NotFound($"Pet not found");
+            }
+
+            return Ok(pet);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500);
+        }
+    }
+}
