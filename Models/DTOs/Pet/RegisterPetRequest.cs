854d4976a0d70001df1de258d8c64bbbbd439e3c
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-05-13 15:30:30 +0200

    Feat: Add validation data annotations to RegisterPetRequest.

--- Models/DTOs/Pet/RegisterPetRequest.cs
+++ Models/DTOs/Pet/RegisterPetRequest.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 public class RegisterPetRequest
 {
-    public string Name { get; set; }
+    [Required(ErrorMessage = "Name cannot be null")]
+    [StringLength(
+        50,
+        MinimumLength = 3,
+        ErrorMessage = "The pet name must be between 3 and 50 characters."
+    )]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Age cannot be null")]
+
     public int Age { get; set; }
     public Gender Gender { get; set; } = Gender.Unknown;
     public Species Species { get; set; } = Species.Undefined;
