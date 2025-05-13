759f9a87b4a37a6c1a8620cc3f16fe37bc3da78d
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-13 14:43:15 +0200

    feat: add DoesShelterExistForUserAsync method signature to IShelterService

--- Data/Repositories/Interfaces/IShelterRepository.cs
+++ Data/Repositories/Interfaces/IShelterRepository.cs
@@ -3,4 +3,5 @@ public interface IShelterRepository
 {
     Task<ShelterEntity> CreateShelterAsync(ShelterEntity newShelter);
     Task<ShelterEntity?> GetShelterByUserIdAsync(string userId);
+    Task<bool> DoesShelterExistForUserAsync(string userId);
 }
