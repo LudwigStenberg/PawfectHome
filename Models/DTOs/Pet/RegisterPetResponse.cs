81132f0e97c192ac2df6cb8629eca1ead4cac09d
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-05-14 10:20:02 +0200

    Feat: Improve RegisterPetResponse with more details

--- Models/DTOs/Pet/RegisterPetResponse.cs
+++ Models/DTOs/Pet/RegisterPetResponse.cs
@@ -1,5 +1,15 @@
 public class RegisterPetResponse
 {
-    public int PetId { get; set; }
-    public string Name { get; set; }
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required DateTime Birthdate { get; set; }
+    public required Gender Gender { get; set; }
+    public required Species Species { get; set; }
+    public required string Breed { get; set; }
+    public required string Description { get; set; }
+    public string? ImageURL { get; set; }
+    public required bool IsNeutered { get; set; }
+    public required bool HasPedigree { get; set; }
+    public required int ShelterId { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
