a5542c99bbe914518340397540fc1d9985f2d734
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-14 14:53:44 +0200

    feat: define GetAllShelters signature in IShelterService

--- Services/Interfaces/IShelterService.cs
+++ Services/Interfaces/IShelterService.cs
@@ -3,4 +3,5 @@ public interface IShelterService
 {
     Task<RegisterShelterResponse> RegisterShelterAsync(string userId, RegisterShelterRequest request);
     Task<ShelterResponse> GetShelterAsync(int id);
+    Task<ICollection<ShelterResponse>> GetAllShelters();
 }
