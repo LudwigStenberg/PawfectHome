3973478ca30ac6d36b308c9f9f17a4b0ff106f25
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-14 10:18:17 +0200

    feat: define GetShelterAsync signature in IShelterService

--- Services/Interfaces/IShelterService.cs
+++ Services/Interfaces/IShelterService.cs
@@ -2,4 +2,5 @@
 public interface IShelterService
 {
     Task<ShelterResponse> RegisterShelterAsync(string userId, RegisterShelterRequest request);
+    Task<ShelterResponse> GetShelterAsync(int id);
 }
