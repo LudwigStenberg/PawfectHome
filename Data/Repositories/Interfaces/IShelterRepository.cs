38e3448396a862f9e5f47174e029c8ba76db261f
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-14 16:11:16 +0200

    feat: define method signature for FetchAllSheltersAsync() in IShelterRepository

--- Data/Repositories/Interfaces/IShelterRepository.cs
+++ Data/Repositories/Interfaces/IShelterRepository.cs
@@ -4,5 +4,6 @@ public interface IShelterRepository
     Task<ShelterEntity> CreateShelterAsync(ShelterEntity newShelter);
     Task<ShelterEntity?> FetchShelterByIdAsync(int id);
     Task<ShelterEntity?> FetchShelterByUserIdAsync(string userId);
+    Task<ICollection<ShelterWithPetCountDto>> FetchAllSheltersAsync();
     Task<bool> DoesShelterExistForUserAsync(string userId);
 }
