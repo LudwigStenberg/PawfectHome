f387508b718aa6d754dc56e457109f1553a8d449
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-14 07:52:27 +0200

    Add: XML comments for

--- Services/PetService.cs
+++ Services/PetService.cs
@@ -4,10 +4,22 @@ public class PetService : IPetService
 {
     private readonly AppDbContext dbContext;
 
+    /// <summary>
+    /// Initialize instance of the PetService class.
+    /// </summary>
+    /// <param name="appdbContext"> The database context for pet operations.</param>
+
     public PetService(AppDbContext appdbContext)
     {
         dbContext = appdbContext;
     }
+
+    /// <summary>
+    /// Fetch specific pet from database using id.
+    /// </summary>
+    /// <param name="id">unique identifier of specific pet to be retrieved.</param>
+    /// <returns>the task result contains the pet entity</returns>
+    /// <exception cref="KeyNotFoundException"> Throw when no pet with the specified id is found.</exception>
 
     public async Task<PetEntity> GetPetAsync(int id)
     {
