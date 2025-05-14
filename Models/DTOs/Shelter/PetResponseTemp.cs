117a684780d6d6a1096ed8a886a971e5fab1d07d
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-14 11:20:05 +0200

    fix: PetResponseTemp: change from string to enum for 'Species'

--- Models/DTOs/Shelter/PetResponseTemp.cs
+++ Models/DTOs/Shelter/PetResponseTemp.cs
@@ -2,5 +2,5 @@ public class PetResponseTemp
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Species { get; set; }
+    public Species Species { get; set; }
 }
