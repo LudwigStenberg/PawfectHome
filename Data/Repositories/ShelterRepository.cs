9a62c8c01d1df2def85cd8cf2e8493c1a9594faa
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-14 16:31:19 +0200

    feat: implement FetchAllSheltersAsync() in ShelterRepository

--- Data/Repositories/ShelterRepository.cs
+++ Data/Repositories/ShelterRepository.cs
@@ -29,6 +29,20 @@ public class ShelterRepository : IShelterRepository
             .SingleOrDefaultAsync(s => s.UserId == userId);
     }
 
+    public async Task<ICollection<ShelterWithPetCountDto>> FetchAllSheltersAsync()
+    {
+        return await context.Shelters
+            .Select(s => new ShelterWithPetCountDto
+            {
+                Id = s.Id,
+                Name = s.Name,
+                Description = s.Description,
+                Email = s.Email,
+                PetCount = s.Pets.Count
+
+            }).ToListAsync();
+    }
+
     public async Task<bool> DoesShelterExistForUserAsync(string userId)
     {
         return await context.Shelters.AnyAsync(s => s.UserId == userId);
