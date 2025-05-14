36bc8b7c21cefeac2d0e4f539951f64a8415bbf0
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-14 19:54:20 +0200

    feat: create ShelterUpdateRequest DTO

---
+++ Models/DTOs/Shelter/ShelterUpdateRequest.cs
@@ -0,0 +1,7 @@
+public class ShelterUpdateRequest
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Email { get; set; }
+}
