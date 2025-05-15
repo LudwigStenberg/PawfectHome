672b29b3810976e337a08edc08531ac30d117c2b
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-15 08:50:52 +0200

    Refactoring, removed duplicate file

--- Models/DTOs/Pet/GetPetResponse.cs
+++ Models/DTOs/Pet/GetPetResponse.cs
@@ -2,7 +2,7 @@ public class GetPetResponse
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public int Age { get; set; }
+    public DateTime Birthdate { get; set; }
     public Gender Gender { get; set; }
     public Species Species { get; set; }
     public string Breed { get; set; }
