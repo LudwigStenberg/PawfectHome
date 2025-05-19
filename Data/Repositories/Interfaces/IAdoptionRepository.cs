25f5ce2f4b8c459c8891a6f242f9ec17db7abef8
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-19 13:22:28 +0200

    feat: add method signature in IAdoptionRepository

--- Data/Repositories/Interfaces/IAdoptionRepository.cs
+++ Data/Repositories/Interfaces/IAdoptionRepository.cs
@@ -1,4 +1,5 @@
 public interface IAdoptionRepository
 {
     Task<AdoptionApplicationEntity> CreateAdoptionAsync(AdoptionApplicationEntity newAdoptionApplication);
+    Task RemoveAdoptionApplicationAsync(int id, string userId);
 }
