3fe5c9fd83e12553fba9b09156e44dab89e2659f
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-19 13:12:56 +0200

    Feat: Add UserService and interface. Add method for fecting user with id

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -1,1 +1,4 @@
-public class IUserService { }
+public class IUserService
+{
+    public Task<GetUserReponse> GetUserAsync(int id);
+}
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,1 +1,9 @@
-public class UserService : IUserService { }
+public class UserService : IUserService
+{
+    public UserService(IUserRepository userRepository, ILogger logger, AppDbContext appDbContext);
+
+    public async Task<GetUserReponse> GetUserAsync(int id)
+    {
+        var user = await userRepository.FetchUserAsync(id);
+    }
+}
