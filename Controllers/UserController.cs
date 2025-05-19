183f41b9e038bbe866edc5046f246bc02b822ca1
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-19 13:10:04 +0200

    Feature: Add Get for UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,5 +12,21 @@ public class UsersController : ControllerBase
         this.userService = userService;
     }
 
-    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        try
+        {
+            var userResponse = await userService.GetUserAsync;
+            return Ok(userResponse);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "An error occured while fetching user  ");
+        }
+    }
 }
