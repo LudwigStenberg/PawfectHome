4ed5a5c38ffb887166d269316a6067b3378b980f
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-20 16:33:12 +0200

    fix: formatting

--- Services/Interfaces/IAdoptionService.cs
+++ Services/Interfaces/IAdoptionService.cs
@@ -1,8 +1,8 @@
 public interface IAdoptionService
 {
     public Task<RegisterAdoptionResponse> RegisterAdoptionApplicationAsync(RegisterAdoptionRequest request);
-    Task RemoveAdoptionApplicationAsync(int id, string userId);
 
     public Task<GetAdoptionApplicationResponse> GetAdoptionApplicationAsync(GetAdoptionApplicationRequest request);
+    Task RemoveAdoptionApplicationAsync(int id, string userId);
 
 }
