f0cfff9ad4fef9d7982325b8c95280b8bf38ff35
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-05-21 18:06:55 +0200

    fix: Add userId to RegisterPetAsync method.     - This allows RegisterPetAsync to access the     userId from the token value.

--- Services/Interfaces/IPetService.cs
+++ Services/Interfaces/IPetService.cs
@@ -1,8 +1,7 @@
 public interface IPetService
 {
     Task<GetPetResponse> GetPetAsync(int id);
-    Task<RegisterPetResponse> RegisterPetAsync(RegisterPetRequest request);
-
+    Task<RegisterPetResponse> RegisterPetAsync(RegisterPetRequest request, string userId);
     Task<IEnumerable<GetPetResponse>> GetAllPetsAsync();
     Task RemovePetAsync(int id, string userId);
 }
