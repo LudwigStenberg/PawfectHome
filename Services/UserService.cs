b68b06517fd038a39f9739f42edb9aeb1adf07d7
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-21 10:17:18 +0200

    Add: potential change to code with a TODO comment

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -75,3 +75,4 @@ public class UserService : IUserService
         await userRepository.DeleteUserAsync(id);
     }
 }
+//TODO Maybe move out claims to controller and instead send current userid to service instead of all metadata related to user.
