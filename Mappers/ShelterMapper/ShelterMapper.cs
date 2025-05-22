16daa11be770698bcbc79027cdce28f38908981b
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-22 12:19:45 +0200

    feat: implement ToEntity in ShelterMapper

--- Mappers/ShelterMapper/ShelterMapper.cs
+++ Mappers/ShelterMapper/ShelterMapper.cs
@@ -1,4 +1,13 @@
 public static class ShelterMapper
 {
-
+    public static ShelterEntity ToEntity(RegisterShelterRequest request, string userId)
+    {
+        return new ShelterEntity
+        {
+            Name = request.Name,
+            Description = request.Description ?? "No descrpiton",
+            Email = request.Email,
+            UserId = userId
+        };
+    }
 }
