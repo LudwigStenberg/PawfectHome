13fe15ff2a4b4091965af189694f690afc6b001f
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-23 14:36:08 +0200

    refactor: move ShelterSummary to its own file in DTOs/Shelter

---
+++ Models/DTOs/Shelter/ShelterSummary.cs
@@ -0,0 +1,7 @@
+public class ShelterSummary
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
