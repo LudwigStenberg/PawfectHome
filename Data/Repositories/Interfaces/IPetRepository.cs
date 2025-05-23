d121e032785f06ff09b3387efa5eff8d75430489
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-05-23 14:54:24 +0200

    feat: add UpdatePetAsync to PetRepository

--- Data/Repositories/Interfaces/IPetRepository.cs
+++ Data/Repositories/Interfaces/IPetRepository.cs
@@ -1,9 +1,8 @@
 public interface IPetRepository
 {
     Task<PetEntity> FetchPetAsync(int petId);
-
     public Task<PetEntity> CreatePetAsync(PetEntity petEntity);
-
     Task<IEnumerable<PetEntity>> FetchAllPetsAsync();
+    Task UpdatePetAsync(PetEntity existingPet);
     Task DeletePetAsync(PetEntity petEntity);
 }
