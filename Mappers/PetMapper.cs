f0a9bd689a54855e411acc64469e6e47b604ccd8
LudwigStenberg <lludwigstenberg@gmail.com>
2025-05-23 13:44:40 +0200

    feat: implement ToEntity in PetMapper - includes utchbirthdate param

--- Mappers/PetMapper.cs
+++ Mappers/PetMapper.cs
@@ -1,6 +1,23 @@
 
 public static class PetMapper
 {
+    public static PetEntity ToEntity(RegisterPetRequest request, DateTime utcBirthdate)
+    {
+        return new PetEntity
+        {
+            Name = request.Name,
+            Birthdate = utcBirthdate,
+            Gender = request.Gender,
+            Species = request.Species,
+            Breed = request.Breed,
+            Description = request.Description,
+            ImageURL = request.ImageURL,
+            IsNeutered = request.IsNeutured,
+            HasPedigree = request.HasPedigree,
+            ShelterId = request.ShelterId,
+        };
+    }
+
     public static PetSummaryResponse ToPetSummaryResponse(PetEntity pet)
     {
         return new PetSummaryResponse
