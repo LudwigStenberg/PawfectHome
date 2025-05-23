f8d8e1f1741d240398dfd2cba596b34fe1d52f9b
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-05-23 14:57:00 +0200

    fix: Fix faulty StringLength annotation for RegisterPetRequest

--- Models/DTOs/Pet/RegisterPetRequest.cs
+++ Models/DTOs/Pet/RegisterPetRequest.cs
@@ -17,7 +17,7 @@ public class RegisterPetRequest
     public Species Species { get; set; } = Species.Undefined;
     public string Breed { get; set; } = "Undefined";
 
-    [StringLength(maximumLength: 1000)]
+    [StringLength(1000)]
     public string Description { get; set; } = "No description";
     public string? ImageURL { get; set; }
     public bool IsNeutured { get; set; } = false;
