73be1ed80410eda4576dd2418a0c9c7c0f3e0ef8
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-30 11:18:01 +0200

    Feat: add required to applicantName and pet in sheltersymmry dto. Updated Migrations

--- Models/DTOs/Adoption/AdoptionApplicationShelterSummary.cs
+++ Models/DTOs/Adoption/AdoptionApplicationShelterSummary.cs
@@ -3,7 +3,7 @@ public class AdoptionApplicationShelterSummary
     public int Id { get; set; }
     public DateTime CreatedDate { get; set; }
     public AdoptionStatus AdoptionStatus { get; set; }
-    public string ApplicantName { get; set; }
-    public string PetName { get; set; }
+    public required string ApplicantName { get; set; }
+    public required string PetName { get; set; }
     public int PetId { get; set; }
 }
