1f0222677448b7e094b2b8f61dc59d13ab1ee867
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-02 19:40:25 +0200

    Fix: FetchApplication nullability and refactoring
    - Re-add nullability

--- Data/Repositories/Interfaces/IAdoptionRepository.cs
+++ Data/Repositories/Interfaces/IAdoptionRepository.cs
@@ -3,7 +3,7 @@ public interface IAdoptionRepository
     Task<AdoptionApplicationEntity> CreateAdoptionApplicationAsync(
         AdoptionApplicationEntity newAdoptionApplication
     );
-    Task<AdoptionApplicationEntity> FetchAdoptionApplicationByIdAsync(int id);
+    Task<AdoptionApplicationEntity?> FetchAdoptionApplicationByIdAsync(int id);
     Task<IEnumerable<AdoptionApplicationEntity>> FetchAllAdoptionsAsync(string userId);
     Task<IEnumerable<AdoptionApplicationEntity>> FetchAllShelterAdoptionsAsync(string userId);
     public Task<AdoptionApplicationEntity> UpdateAdoptionStatusAsync(
